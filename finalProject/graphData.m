ee88330dcbc19d4be6db9a99d55df8e8397ccbab
Suphakan <susu8167@colorado.edu>
2020-04-28 15:35:13 -0600

    final commit(implementation and plot data)

--- finalProject/graphData.m
+++ finalProject/graphData.m
@@ -53,14 +53,14 @@ y=data(:,2); % reading its second column of csv file
 
 figure;
 yyaxis left;
-plot(x,y);
+plot(x,y,'-g');
 hold on;
 
 data=csvread("search_hashChaining.csv"); % reading csv file 
 x=data(:,1); % reading its first column
 y=data(:,2); % reading its second column of csv file
 
-plot(x,y)
+plot(x,y,'-r')
 legend("insert","search");
 title("hashChaining");
 xlabel("index (x100)");
@@ -71,12 +71,12 @@ yyaxis right;
 data = csvread("insertCollision_hashChaining.csv");
 x=data(:,1);
 y=data(:,2);
-plot(x,y);
+plot(x,y,'-b');
 
 data = csvread("searchCollision_hashChaining.csv");
 x=data(:,1);
 y=data(:,2);
-plot(x,y);
+plot(x,y,'-y');
 ylabel("number of collisions");
 
 legend("insert","search","insertCollision","searchCollision");
@@ -94,14 +94,14 @@ y=data(:,2); % reading its second column of csv file
 figure;
 
 yyaxis left;
-plot(x,y);
+plot(x,y,'g-');
 hold on;
 
 data=csvread("search_hashLinear.csv"); % reading csv file 
 x=data(:,1); % reading its first column
 y=data(:,2); % reading its second column of csv file
 
-plot(x,y)
+plot(x,y,'-r')
 legend("insert","search");
 title("hashLinear");
 xlabel("index (x100)");
@@ -112,12 +112,12 @@ yyaxis right;
 data = csvread("insertCollision_hashLinear.csv");
 x=data(:,1);
 y=data(:,2);
-plot(x,y);
+plot(x,y,'-b');
 
 data = csvread("searchCollision_hashLinear.csv");
 x=data(:,1);
 y=data(:,2);
-plot(x,y);
+plot(x,y,'-y');
 ylabel("number of collisions");
 
 legend("insert","search","insertCollision","searchCollision");
@@ -136,29 +136,30 @@ y=data(:,2); % reading its second column of csv file
 figure;
 
 yyaxis left;
-plot(x,y);
+plot(x,y,'g-');
 hold on;
 
 data=csvread("search_hashQuadratic.csv"); % reading csv file 
 x=data(:,1); % reading its first column
 y=data(:,2); % reading its second column of csv file
 
-plot(x,y)
+plot(x,y,'r-')
 title("hashQuadratic");
 xlabel("index (x100)");
 ylabel("time consumed");
+ylim([10^-10,10^-7])
 
 yyaxis right;
 
 data = csvread("insertCollision_hashQuadratic.csv");
 x=data(:,1);
 y=data(:,2);
-plot(x,y);
+plot(x,y,'b-');
 
 data = csvread("searchCollision_hashQuadratic.csv");
 x=data(:,1);
 y=data(:,2);
-plot(x,y);
+plot(x,y,'y-');
 ylabel("number of collisions");
 
 legend("insert","search","insertCollision","searchCollision");
--- finalProject/graphHashRescale.m
+++ finalProject/graphHashRescale.m
@@ -1,86 +1,36 @@
-data=csvread("insert_linkedList.csv"); % reading csv file 
-x=data(:,1); % reading its first column
-y=data(:,2); % reading its second column of csv file
-
-figure;
-plot(x,y);
-hold on;
-
-data=csvread("search_linkedList.csv"); % reading csv file 
-x=data(:,1); % reading its first column
-y=data(:,2); % reading its second column of csv file
-
-hold on;
-plot(x,y)
-legend("insert","search");
-title("LinkedList");
-xlabel("index (x100)");
-ylabel("time consumed");
-
-
-
-
-
-
-data=csvread("insert_bst.csv"); % reading csv file 
-x=data(:,1); % reading its first column
-y=data(:,2); % reading its second column of csv file
-
-figure;
-plot(x,y);
-hold on;
-
-data=csvread("search_BST.csv"); % reading csv file 
-x=data(:,1); % reading its first column
-y=data(:,2); % reading its second column of csv file
-
-hold on;
-plot(x,y)
-legend("insert","search");
-title("BST");
-xlabel("index (x100)");
-ylabel("time consumed");
-
-
-
-
-
-
-
 data=csvread("insert_hashChaining.csv"); % reading csv file 
 x=data(:,1); % reading its first column
 y=data(:,2); % reading its second column of csv file
 
 figure;
-plot(x,y);
+yyaxis left;
+plot(x,y,'-g');
 hold on;
 
 data=csvread("search_hashChaining.csv"); % reading csv file 
 x=data(:,1); % reading its first column
 y=data(:,2); % reading its second column of csv file
 
-plot(x,y)
+plot(x,y,'-r')
 legend("insert","search");
 title("hashChaining");
 xlabel("index (x100)");
 ylabel("time consumed");
-
-data = csvread("searchCollision_hashChaining.csv");
-x=data(:,1);
-y=data(:,2);
+ylim([10^-10,10^-7]);
 
 yyaxis right;
 
 data = csvread("insertCollision_hashChaining.csv");
 x=data(:,1);
 y=data(:,2);
-plot(x,y);
+plot(x,y,'-b');
 
 data = csvread("searchCollision_hashChaining.csv");
 x=data(:,1);
 y=data(:,2);
-plot(x,y);
-ylabel("number of collision");
+plot(x,y,'-y');
+ylabel("number of collisions");
+ylim([0,100]);
 
 legend("insert","search","insertCollision","searchCollision");
 
@@ -95,31 +45,35 @@ x=data(:,1); % reading its first column
 y=data(:,2); % reading its second column of csv file
 
 figure;
-plot(x,y);
+
+yyaxis left;
+plot(x,y,'g-');
 hold on;
 
 data=csvread("search_hashLinear.csv"); % reading csv file 
 x=data(:,1); % reading its first column
 y=data(:,2); % reading its second column of csv file
 
-plot(x,y)
+plot(x,y,'-r')
 legend("insert","search");
 title("hashLinear");
 xlabel("index (x100)");
 ylabel("time consumed");
+ylim([10^-10,10^-7]);
 
 yyaxis right;
 
 data = csvread("insertCollision_hashLinear.csv");
 x=data(:,1);
 y=data(:,2);
-plot(x,y);
+plot(x,y,'-b');
 
 data = csvread("searchCollision_hashLinear.csv");
 x=data(:,1);
 y=data(:,2);
-plot(x,y);
-ylabel("number of collision");
+plot(x,y,'-y');
+ylabel("number of collisions");
+ylim([0,100]);
 
 legend("insert","search","insertCollision","searchCollision");
 
@@ -137,30 +91,31 @@ y=data(:,2); % reading its second column of csv file
 figure;
 
 yyaxis left;
-plot(x,y);
+plot(x,y,'g-');
 hold on;
 
 data=csvread("search_hashQuadratic.csv"); % reading csv file 
 x=data(:,1); % reading its first column
 y=data(:,2); % reading its second column of csv file
 
-plot(x,y)
+plot(x,y,'r-')
 title("hashQuadratic");
 xlabel("index (x100)");
 ylabel("time consumed");
+ylim([10^-10,10^-7]);
 
 yyaxis right;
 
 data = csvread("insertCollision_hashQuadratic.csv");
 x=data(:,1);
 y=data(:,2);
-plot(x,y);
+plot(x,y,'b-');
+ylim([0,100]);
 
 data = csvread("searchCollision_hashQuadratic.csv");
 x=data(:,1);
 y=data(:,2);
-plot(x,y);
-ylabel("number of collision");
+plot(x,y,'y-');
+ylabel("number of collisions");
 
 legend("insert","search","insertCollision","searchCollision");
-
